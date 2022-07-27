d4b8d985c83fd37a21f9f8f0fe3419bcfd9a8dca
Indrajit Pal <palindrajit10@gmail.com>
2022-07-27 16:56:10 +0530

    newsapp

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+export class NewsItem extends Component {
+  static propTypes = {}
+
+  render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
+    return (
+        <>
+        <div className="my-3">
+            <div className = "card" style = {{width: "18rem"}}>
+                <img src = {!imageUrl?"https://hot-town-images.s3.amazonaws.com/kwtv/production/2022/January/19/breaking-news.1642620193378.jpeg":imageUrl} className="card-img-top" alt = "error"/>
+                <div className = "card-body">
+                    <h5 className = "card-title">{title}</h5>
+                    <p className = "card-text">{description}</p>
+                    <a href = {newsUrl} target = "_blank" className="btn btn-danger btn-sm">Read More</a>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+  }
+}
+
+export default NewsItem
