50a35e9e805d5a9553878d81135e931e052d2946
Indrajit Pal <palindrajit10@gmail.com>
2022-07-28 16:36:00 +0530

    Navbar centered

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,8 +7,8 @@ export class Navbar extends Component {
   render() {
     return (
         <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid my-container-fluid">
           <a className="navbar-brand" href="/">NewsMonkey</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -20,6 +20,22 @@ export class Navbar extends Component {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/about">About</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  All Categories
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Business</a></li>
+                  <li><a className="dropdown-item" href="#">Entertainment</a></li>
+                  <li><a className="dropdown-item" href="#">General</a></li>
+                  <li><a className="dropdown-item" href="#">Health</a></li>
+                  <li><a className="dropdown-item" href="#">Science</a></li>
+                  <li><a className="dropdown-item" href="#">Sports</a></li>
+                  <li><a className="dropdown-item" href="#">Technology</a></li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
               </li>
             </ul>
           </div>
